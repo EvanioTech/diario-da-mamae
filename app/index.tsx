e687249a07e616ed52eb98f457373beb6f7c8e51
Evanio Tech <evanioshark@gmail.com>
2025-09-09 09:20:52 -0300

    pagina inicial criada

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,14 @@
-import { View,Text,Image } from "react-native";
+import { View,Text,Image, TouchableOpacity } from "react-native";
 
 export default function Index() {
   return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-      <Image source={require('../assets/images/g.png')} style={{width:200,height:200,marginBottom:20}} />
-      <Text style={{fontSize:24,fontWeight:'bold'}}>Welcome to the App!</Text>
-        <Text style={{fontSize:16,color:'#666'}}>This is the main entry point of the application.</Text>
+      <Image source={require('../assets/images/gr.png')} style={{width:350,height:350,marginBottom:20}} />
+      <Text style={{fontSize:24,fontWeight:'bold'}}>Seja Bem vinda Mamãe!</Text>
+        <Text style={{fontSize:16,color:'#666',marginBottom:20}}>Seu diário digital para acompanhar sua jornada.</Text>
+        <TouchableOpacity style={{backgroundColor:'#cc11a3ff',padding:12,borderRadius:8,width:'50%',alignItems:'center', marginTop:20}}>
+          <Text style={{color:'#fff',fontWeight:'bold'}}>Começar</Text>
+        </TouchableOpacity>
     </View>
   );
 }
