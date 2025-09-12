58bd511342316fac9bc80a12ee33d609ecef7595
Evanio Tech <evanioshark@gmail.com>
2025-09-12 10:20:13 -0300

    mudança na tab explore

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,15 +1,105 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image } from 'react-native';
 
 const ExploreScreen = () => {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>Explore</Text>
-      <Text style={styles.subtitle}>
-        Discover new content, tips, and resources for your journey!
-      </Text>
-      {/* Add your explore content here */}
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Minhas Anotações</Text>
+      
+      
+              
+                <TouchableOpacity onPress={() => alert('Cor 1 selecionada!')} 
+                style={
+                  {width:"95%",
+                    height:70,
+                   padding:20,
+                    backgroundColor:'#f539ccff',
+                     borderRadius:10,
+                     flexDirection:'row',
+                    
+                     alignItems:'center',
+                      justifyContent:'center',
+                      marginTop:150,
+                      marginBottom:20,
+                     
+                     
+                    
+                     
+                     }}>
+                      <Image source={require('../../assets/images/mama.png')} style={{width:40, height:40, marginRight:10}} />
+                  <Text style={{color:'#fff', fontWeight:'bold',fontSize:26}}>Tete</Text>
+                </TouchableOpacity>
+              
+              
+                <TouchableOpacity onPress={() => alert('Cor 1 selecionada!')} 
+                style={
+                  {width:"95%",
+                   height:70,
+                    backgroundColor:'#f539ccff',
+                     borderRadius:10,
+                     
+                     alignSelf:'center',
+                     alignItems:'center',
+                      justifyContent:'center',
+                      flexDirection:'row',
+                      marginBottom:20,
+                      
+                      
+                     
+                     }}>
+                      <Image source={require('../../assets/images/coco.png')} style={{width:40, height:40, marginRight:10}} />
+                  <Text style={{color:'#fff', fontWeight:'bold',fontSize:26}}>Coco</Text>
+                </TouchableOpacity>
+              
+              
+            
+            
+              
+                <TouchableOpacity onPress={() => alert('Cor 1 selecionada!')} 
+                style={
+                  {width:"95%",
+                    height:70,
+                   padding:20,
+                    backgroundColor:'#f539ccff',
+                     borderRadius:10,
+                    
+                     alignItems:'center',
+                      justifyContent:'center',
+                      flexDirection:'row',
+                      marginBottom:20,
+                     
+                     
+                    
+                     
+                     }}>
+                      <Image source={require('../../assets/images/banho.png')} style={{width:40, height:40, marginRight:10}} />
+                  <Text style={{color:'#fff', fontWeight:'bold',fontSize:26}}>Banho</Text>
+                </TouchableOpacity>
+              
+              
+                <TouchableOpacity onPress={() => alert('Cor 1 selecionada!')} 
+                style={
+                  {width:"95%",
+                   height:70,
+                    backgroundColor:'#f539ccff',
+                     borderRadius:10,
+                      
+                     
+                     alignSelf:'center',
+                     alignItems:'center',
+                      justifyContent:'center',
+                      flexDirection:'row',
+                      
+                      
+                     
+                     }}>
+                      <Image source={require('../../assets/images/frasco.png')} style={{width:40, height:40, marginRight:10}} />
+                  <Text style={{color:'#fff', fontWeight:'bold',fontSize:26}}>Remédio</Text>
+                </TouchableOpacity>
+              
+              
+            
+    </View>
   );
 };
 
@@ -24,6 +114,7 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: 'bold',
     marginVertical: 16,
+    marginTop: 90,
   },
   subtitle: {
     fontSize: 18,
