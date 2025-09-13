a0b00a4c5a5d2c101868e27afdb02f0232e05206
Evanio Tech <evanioshark@gmail.com>
2025-09-13 09:21:15 -0300

    ajuste no style do explore

--- app/(tabs)/explore/index.tsx
+++ app/(tabs)/explore/index.tsx
@@ -2,6 +2,8 @@ import { router } from 'expo-router';
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image } from 'react-native';
 
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 const ExploreScreen = () => {
   return (
     <View style={styles.container}>
@@ -12,23 +14,24 @@ const ExploreScreen = () => {
                 <TouchableOpacity onPress={() => router.push('/(tabs)/explore/tete')} 
                 style={
                   {width:"95%",
-                    height:70,
-                   padding:20,
-                    backgroundColor:'#f539ccff',
+                   height:70,
+                    backgroundColor:'#fff',
                      borderRadius:10,
-                     flexDirection:'row',
-                    
-                     alignItems:'center',
-                      justifyContent:'center',
-                      marginTop:150,
+                     borderColor:'#fa0fb3ff',
+                     borderWidth:2,
+                     alignSelf:'center',
+                      alignItems:'center',
+                      padding:10,
                       marginBottom:20,
-                     
-                     
-                    
-                     
-                     }}>
-                      <Image source={require('../../../assets/images/mama.png')} style={{width:40, height:40, marginRight:10}} />
-                  <Text style={{color:'#fff', fontWeight:'bold',fontSize:26}}>Tete</Text>
+                      marginTop:70,
+                      
+                      flexDirection:'row',
+                      
+
+
+                     }}><Image source={require('../../../assets/images/mama.png')} style={{width:40, height:40, marginRight:10}} />
+                  <Text style={{color:'#fa0fb3ff', fontWeight:'bold',fontSize:26}}>Tete</Text>
+                  <AntDesign name="arrowright" size={28} color="black" style={{ justifyContent: 'flex-end', marginLeft: 'auto' }} />
                 </TouchableOpacity>
               
               
@@ -36,20 +39,22 @@ const ExploreScreen = () => {
                 style={
                   {width:"95%",
                    height:70,
-                    backgroundColor:'#f539ccff',
+                    backgroundColor:'#fff',
                      borderRadius:10,
-                     
+                     borderColor:'#fa0fb3ff',
+                     borderWidth:2,
                      alignSelf:'center',
-                     alignItems:'center',
-                      justifyContent:'center',
-                      flexDirection:'row',
+                      alignItems:'center',
+                      padding:10,
                       marginBottom:20,
                       
+                      flexDirection:'row',
                       
-                     
-                     }}>
-                      <Image source={require('../../../assets/images/coco.png')} style={{width:40, height:40, marginRight:10}} />
-                  <Text style={{color:'#fff', fontWeight:'bold',fontSize:26}}>Coco</Text>
+
+
+                     }}><Image source={require('../../../assets/images/coco.png')} style={{width:40, height:40, marginRight:10}} />
+                  <Text style={{color:'#fa0fb3ff', fontWeight:'bold',fontSize:26}}>Coco</Text>
+                  <AntDesign name="arrowright" size={28} color="black" style={{ justifyContent: 'flex-end', marginLeft: 'auto' }} />
                 </TouchableOpacity>
               
               
@@ -59,22 +64,24 @@ const ExploreScreen = () => {
                 <TouchableOpacity onPress={() => router.push('/(tabs)/explore/banho')} 
                 style={
                   {width:"95%",
-                    height:70,
-                   padding:20,
-                    backgroundColor:'#f539ccff',
+                   height:70,
+                    backgroundColor:'#fff',
                      borderRadius:10,
-                    
-                     alignItems:'center',
-                      justifyContent:'center',
-                      flexDirection:'row',
+                     borderColor:'#fa0fb3ff',
+                     borderWidth:2,
+                     alignSelf:'center',
+                      alignItems:'center',
+                      padding:10,
                       marginBottom:20,
-                     
-                     
-                    
+                      
+                      flexDirection:'row',
+                      
+                      
                      
                      }}>
                       <Image source={require('../../../assets/images/banho.png')} style={{width:40, height:40, marginRight:10}} />
-                  <Text style={{color:'#fff', fontWeight:'bold',fontSize:26}}>Banho</Text>
+                  <Text style={{color:'#fa0fb3ff', fontWeight:'bold',fontSize:26}}>Banho</Text>
+                  <AntDesign name="arrowright" size={28} color="black" style={{ justifyContent: 'flex-end', marginLeft: 'auto' }} />
                 </TouchableOpacity>
               
               
@@ -82,20 +89,22 @@ const ExploreScreen = () => {
                 style={
                   {width:"95%",
                    height:70,
-                    backgroundColor:'#f539ccff',
+                    backgroundColor:'#fff',
                      borderRadius:10,
-                      
-                     
+                     borderColor:'#fa0fb3ff',
+                     borderWidth:2,
                      alignSelf:'center',
-                     alignItems:'center',
-                      justifyContent:'center',
+                      alignItems:'center',
+                      padding:10,
+                      
                       flexDirection:'row',
                       
                       
                      
                      }}>
-                      <Image source={require('../../../assets/images/frasco.png')} style={{width:40, height:40, marginRight:10}} />
-                  <Text style={{color:'#fff', fontWeight:'bold',fontSize:26}}>Remédio</Text>
+                      <Image source={require('../../../assets/images/frasco.png')} style={{width:40, height:40, marginRight:10, marginLeft:10}} />
+                  <Text style={{color:'#fa0fb3ff', fontWeight:'bold',fontSize:26}}>Remédio</Text>
+                  <AntDesign name="arrowright" size={28} color="black" style={{ justifyContent: 'flex-end', marginLeft: 'auto' }} />
                 </TouchableOpacity>
               
               
