04b414ea2a7a90f143abee5da60922cb9a7623f4
Evanio Tech <evanioshark@gmail.com>
2025-09-15 08:21:35 -0300

    ajuste no padding do button report

--- app/(tabs)/profile/report.tsx
+++ app/(tabs)/profile/report.tsx
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     },
     button: {
         backgroundColor: '#fff',
-        padding: 15,
+        padding: 8,
         borderRadius: 8,
         alignItems: 'center',
         borderColor: '#ff69b4',
